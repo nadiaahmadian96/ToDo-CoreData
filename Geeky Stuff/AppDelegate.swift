2a144a046390b54100b90961524b0acb9b38af95
Nadia Ahmadian <nadia.ahmadian96@yahoo.com>
2021-05-07 21:34:14 +0430

    Add files via upload

---
+++ Geeky Stuff/AppDelegate.swift
@@ -0,0 +1,69 @@
+//
+//  AppDelegate.swift
+//  Geeky Stuff
+//
+//  Created by Nadia Ahmadian on 2021-04-14.
+//
+
+import UIKit
+import CoreData
+//import RealmSwift
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+//        do{
+//        let realm = try Realm()
+//        }catch{
+//            print("Error initialising new real, \(error)")
+//        }
+        
+        return true
+        
+    }
+
+    // MARK: UISceneSession Lifecycle
+
+    func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
+
+        return UISceneConfiguration(name: "Default Configuration", sessionRole: connectingSceneSession.role)
+    }
+
+    func application(_ application: UIApplication, didDiscardSceneSessions sceneSessions: Set<UISceneSession>) {
+
+    }
+
+    // MARK: - Core Data stack
+
+    lazy var persistentContainer: NSPersistentContainer = {
+
+        let container = NSPersistentContainer(name: "DataModel")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container
+    }()
+
+    // MARK: - Core Data Saving support
+
+    func saveContext () {
+        let context = persistentContainer.viewContext
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+
+
+}
+
---
+++ Geeky Stuff/Controllers/CategoryViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  CategoryViewCell.swift
+//  Geeky Stuff
+//
+//  Created by Nadia Ahmadian on 2021-05-05.
+//
+
+import UIKit
+
+class CategoryViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
